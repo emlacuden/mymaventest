4ec3327a90d326af9bfe587598f9932fe4152846
emlacuden <hungpp250795@gmail.com>
2018-03-19 16:13:37 +0700

    second commit

--- src/main/java/com/example/demo/MyProjectApplication.java
+++ src/main/java/com/example/demo/MyProjectApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyProjectApplication.class, args);
-	}
+    public static String mainstriung = "ádasd";
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectApplication.class, args);
+    }
 }
